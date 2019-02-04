0c7eedac18b10067789e5578aa29b6f28f359756
Sokureo <pelmenium.sokurium@gmail.com>
2019-02-04 18:37:41 +0300

    stats options, res design, sent number

--- number_sentences.py
+++ number_sentences.py
@@ -11,14 +11,25 @@ for f in files:
     with open('./corpus/conlab_bashkir/' + f, 'r', encoding='utf-8') as fil:
         text = json.loads(fil.read())
     for sent in text['sentences']:
-        if len(str(i)) == 1:
-            number = '00' + str(i)
-        elif len(str(i)) == 2:
-            number = '0' + str(i)
-        else:
-            number = str(i)
-        sent['number'] = number
-        i += 1
+         if sent['lang'] == 0:
+                if len(str(i)) == 1:
+                    number = '00' + str(i)
+                elif len(str(i)) == 2:
+                    number = '0' + str(i)
+                else:
+                    number = str(i)
+                sent['number'] = number
+                i += 1
+                step = i
+         else:
+                if len(str(i-step+1)) == 1:
+                    number = '00' + str(i-step+1)
+                elif len(str(i-step+1)) == 2:
+                    number = '0' + str(i-step+1)
+                else:
+                    number = str(i-step+1)
+                sent['number'] = number
+                i += 1
     with open('./corpus/conlab_bashkir/' + f, 'w', encoding='utf-8') as fw:
         json.dump(text, fw, ensure_ascii=False,
                   indent=1)
